e3706ea7412129943c0662c7e5af7f9a08276ce0
shamil332 <shamilkazimli2006@gmail.com>
2025-12-28 11:09:12 +0000

    task 0

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-whois "$1" | awk '
-/^(Registrant|Admin|Tech)/ {
-  split($0,a,": ")
-  printf "%s,%s\n", a[1], a[2]
-}' > "$1.csv"
+whois "$1" | awk '/^(Registrant|Admin|Tech)/{sub(/^ +/,""); split($0,a,":"); printf "%s,%s\n",a[1],substr($0,index($0,a[2]))}' > "$1.csv"
