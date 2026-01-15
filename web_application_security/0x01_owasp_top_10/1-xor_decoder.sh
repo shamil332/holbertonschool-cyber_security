a0dd05dcf609663ca20e50c51d36da2a9869b352
shamil332 <shamilkazimli2006@gmail.com>
2026-01-15 08:27:22 +0000

    task 1

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 hash="$1"
 clean="${hash#\{xor\}}"
-decoded=$(echo "$clean" | base64 --decode 2>/dev/null)
-result=""
-while IFS= read -r -n1 char; do
+echo "$clean" | base64 --decode 2>/dev/null | while IFS= read -r -n1 char
+do
     byte=$(printf "%d" "'$char")
-    xored=$(( byte ^ 0x5F ))
-    result+=$(printf "\\x%02x" "$xored")
-done <<< "$decoded"
-echo -e "$result"
-
+    xored=$((byte ^ 0x5F))
+    printf "\\x%02x" "$xored"
+done | xargs printf "%b\n"
