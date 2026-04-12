125186ebf73bb214eac923fc5a90dfa8653844d2
shamil332 <shamilkazimli2006@gmail.com>
2026-04-12 15:17:22 +0100

    task 3

--- web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat logs.txt | awk {'print $12'} | sort | uniq -c | sort -r | head -n 1 | awk {'print $2'}
+cat logs.txt | awk {'print $12'} | sort | uniq -c | sort -r | head -n 1 | awk {'print $2'} | tr -d '"'
