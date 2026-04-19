c2218a79b402241c0862e2a563c35ec42e3f3344
shamil332 <shamilkazimli2006@gmail.com>
2026-04-19 15:39:22 +0100

    task 5

--- web_application_security/0x0c_web_application_foresics/5-users.sh
+++ web_application_security/0x0c_web_application_foresics/5-users.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep "new user" auth.log | awk {'print $8'} | awk -F'[=,]' '{print $2}'
+grep "new user" auth.log | awk {'print $8'} | awk -F'[=,]' '{print $2}' | sort | paste -sd ',' -
