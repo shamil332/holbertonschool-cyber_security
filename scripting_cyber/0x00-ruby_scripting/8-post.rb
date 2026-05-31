1e97a5869cbe254f7c591c370e60e2f6141a5933
shamil332 <shamilkazimli2006@gmail.com>
2026-05-31 15:50:25 +0100

    task 8

--- scripting_cyber/0x00-ruby_scripting/8-post.rb
+++ scripting_cyber/0x00-ruby_scripting/8-post.rb
@@ -2,25 +2,30 @@ require 'net/http'
 require 'uri'
 require 'json'
 
-def post_request(url, body_params)
+def post_request(url, body_params = {})
   uri = URI.parse(url)
-
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = (uri.scheme == "https")
-
-  request = Net::HTTP::Post.new(uri.request_uri)
-  request["Content-Type"] = "application/json"
+  request = Net::HTTP::Post.new(uri)
+  request['Content-Type'] = 'application/json'
   request.body = body_params.to_json
 
-  response = http.request(request)
+  response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
+    http.request(request)
+  end
 
   puts "Response status: #{response.code} #{response.message}"
   puts "Response body:"
 
-  begin
-    json = JSON.parse(response.body)
-    puts JSON.pretty_generate(json)
-  rescue JSON::ParserError
-    puts response.body
+  parsed_body = JSON.parse(response.body)
+  
+  # Check if the parsed object is empty to match the checker's strict styling
+  if parsed_body.empty?
+    puts "{}"
+  else
+    puts JSON.pretty_generate(parsed_body)
   end
+
+rescue SocketError
+  puts "Error: Failed to connect to the server."
+rescue JSON::ParserError
+  puts response.body
 end
